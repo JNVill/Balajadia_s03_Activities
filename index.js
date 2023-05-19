cf27dd75ceb223f21f0f1a8212abe0f9447cf10e
Jonas Balajadia <128715065+JNVill@users.noreply.github.com>
2023-05-19 22:10:07 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+let role = prompt("Enter Role: ").toLowerCase();
+let username = prompt("Enter Username: ");
+let password = prompt("Enter Password: ");
+
+function login() {
+    if (typeof username === "string" && typeof role === "string" && typeof password === "string") {
+        switch (role) {
+            case "admin":
+                console.log("Welcome back to the class portal, admin!");
+                break;
+            case "teacher":
+                console.log("Thank you for logging in, teacher!");
+                gradeInput();
+                break;
+            case "student":
+                console.log("Welcome to the class portal, student!");
+                gradeInput();
+                break;
+            default:
+                console.log("Role out of range.");
+                break;
+        }
+    }
+}
+
+function gradeInput() {
+    console.log("\n|Average Grade Calculator|");
+    let grades = [];
+    for (let i = 0; i < 4; i++) {
+        grades.push(parseInt(prompt("Enter score " + (i + 1) + ": ")));
+    }
+    grade(grades);
+}
+
+function grade(grades) {
+    let sum = grades.reduce((total, grade) => total + grade, 0);
+    let average = sum / grades.length;
+
+    if (average >= 96 && average <= 100) {
+        console.log("Hello " + role + ", your average is: " + Math.round(average) + "\nand your grade is A+");
+    } else if (average >= 90 && average <= 95) {
+        console.log("Hello " + role + ", your average is: " + Math.round(average) + "\nand your grade is A");
+    } else if (average >= 85 && average <= 89) {
+        console.log("Hello " + role + ", your average is: " + Math.round(average) + "\nand your grade is B");
+    } else if (average <= 74) {
+        console.log("Hello " + role + ", your average is: " + Math.round(average) + "\nand your grade is F");
+    }
+}
+
+login();
